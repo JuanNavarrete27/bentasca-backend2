d8951adeaefb549e3a31f89d1a735fc954d22259
JuanNavarrete27 <jnavarretealdave@gmail.com>
2025-11-28 14:05:56 -0300

    Update reservas.js

--- routes/reservas.js
+++ routes/reservas.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middlewares/auth');
 const reservasController = require('../controllers/reservasController');
 
-router.post('/', reservasController.crearReserva);
+// Crear reserva sin usuario logeado (invitado)
+router.post('/crear', reservasController.crearReservaInvitado);
+
+// Crear reserva con usuario logeado (usa req.user)
+router.post('/crear-con-usuario', auth, reservasController.crearReservaConUsuario);
+
+// Obtener todas las reservas (más adelante le podés poner soloAdmin si querés)
+router.get('/', reservasController.obtenerReservas);
 
 module.exports = router;
